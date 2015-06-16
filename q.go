66706960951db1c3be00f96fca9d5e055ca7434a
reconditematter <lennykneller@Lennys-MacBook-Pro.local>
2015-06-16 10:55:24 -0700

    Initial commit.

---
+++ q.go
@@ -0,0 +1,86 @@
+// Copyright (c) 2015 Leonid Kneller
+
+package pq
+
+import "math/big"
+
+// Q represents a rational number of arbitrary precision.
+type Q struct {
+	_r *big.Rat
+}
+
+// MkQ returns a rational number equal to r.
+func MkQ(r *big.Rat) Q {
+	if r == nil {
+		return Q{new(big.Rat)}
+	}
+	return Q{new(big.Rat).Set(r)}
+}
+
+func r(x Q) *big.Rat {
+	if x._r == nil {
+		return new(big.Rat)
+	}
+	return x._r
+}
+
+// Neg returns -x.
+func (x Q) Neg() Q {
+	return Q{new(big.Rat).Neg(r(x))}
+}
+
+// Abs returns |x|.
+func (x Q) Abs() Q {
+	return Q{new(big.Rat).Abs(r(x))}
+}
+
+// Inv returns 1/x.
+func (x Q) Inv() Q {
+	return Q{new(big.Rat).Inv(r(x))}
+}
+
+// Add returns x+y.
+func (x Q) Add(y Q) Q {
+	return Q{new(big.Rat).Add(r(x), r(y))}
+}
+
+// Sub returns x-y.
+func (x Q) Sub(y Q) Q {
+	return Q{new(big.Rat).Sub(r(x), r(y))}
+}
+
+// Mul returns x*y.
+func (x Q) Mul(y Q) Q {
+	return Q{new(big.Rat).Mul(r(x), r(y))}
+}
+
+// Div returns x/y.
+func (x Q) Div(y Q) Q {
+	return Q{new(big.Rat).Quo(r(x), r(y))}
+}
+
+// Sgn returns:
+//
+//	-1 if x < 0
+//	 0 if x = 0
+//	+1 if x > 0
+func (x Q) Sgn() int {
+	return r(x).Sign()
+}
+
+// Cmp returns:
+//
+//	-1 if x < y
+//	 0 if x = 0
+//	+1 if x > y.
+func (x Q) Cmp(y Q) int {
+	return r(x).Cmp(r(y))
+}
+
+// Rat returns a big.Rat number equal to x.
+func (x Q) Rat() *big.Rat {
+	if x._r == nil {
+		return new(big.Rat)
+	}
+	return new(big.Rat).Set(x._r)
+}
